83d986ce23427392743e5b69e8294a1730b296ea
Surya Prakash <rspsuriya777@gmail.com>
2020-11-24 13:20:10 +0530

    Export JSON data into excel using JQUERY
    Used as POC for styling excel and more features within JQUERY

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ExcelService } from './services/excelService';
+import reportWebVitals from './reportWebVitals';
 
 function App() {
+
+  const  generateExcel = (e: any) => {
+    const rows = [];
+    const styles  = [];
+    const fileName = 'export.xlsx';
+    const headers = ['Name', 'ID'];
+    const lastCol = headers.length - 1;
+    rows.push([], headers);
+    styles.push(
+      // styles can be applied to row, col or cell, but if you do it more then once, only the latest will apply
+      {col: '0-' + lastCol, width: 20},
+      {row: rows.length - 1, height: 20},
+      {row: rows.length - 1, col: '0-' + lastCol, font: 'Arial 10 #000000 B', wrapText: 1,},
+    );
+    rows.push(['Surya', '777']);
+    const excelObj = new ExcelService();
+    excelObj.generate(fileName, [{sheetTitle: 'Sheet 1', rows: rows, styles: styles}])
+
+  }
+
+  const  generateReportTemplateExcel = (e: any) => {
+    const excelObj = new ExcelService();
+    const rows = [];
+    const styles  = [];
+    const fileName = 'export.xlsx';
+    const reportName = 'Active Assets Report';
+    const headers = ['Name', 'ID', 'Location', 'Category'];
+    const lastCol = headers.length - 1;
+    rows[0][lastCol - 1] = 'Time Stamp';
+    rows[0][lastCol] = excelObj.toExcelLocalTime(new Date());
+    rows.push([], ['REPORT NAME', reportName]);
+
+    rows.push([], ['Location', 'Memphis']);
+
+    rows.push([], ['Report Type', 'Scheduled']);
+
+    rows.push([], ['Status', 'InService']);
+
+    rows.push(['INTERVAL', 'Shiftwise(Shift 1, Shift 2)']);
+    rows.push(['FROM', 'July 4 , 2020']);
+    rows.push(['TO', 'Jan 1, 2020']);
+    rows.push(['FREQUENCY', '1']);
+    rows.push([], headers);
+    
+
+
+    styles.push(
+      // styles can be applied to row, col or cell, but if you do it more then once, only the latest will apply
+      {col: '0-' + lastCol, width: 20},
+      {row: rows.length - 1, height: 20},
+      {row: 0, col: lastCol, font: 'Arial 9 #000000 B', align: 'R C', format: "mmm dd, yyyy;@"},
+      {row: 1, col: 1, font: 'Arial 9 #000000 B', wrapText: 1},
+      {row: 3 , col: 1, font: 'Arial 9 #000000 B', wrapText: 1,},
+      {row: 5 , col: 1, font: 'Arial 9 #000000 B', wrapText: 1,},
+      {row: rows.length - 1, col: '0-' + lastCol, font: 'Arial 10 #000000 B', wrapText: 1,},
+    );
+    for(let i= 0; i < 10 ;i++) {
+      rows.push(['A', 'B', 'C', 'D']);
+    }
+    excelObj.generate(fileName, [{sheetTitle: 'Sheet 1', rows: rows, styles: styles}])
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <button style={{width: 200, height: 200, background: 'green'}} onClick={generateExcel}> 
+      Generate Excel
+      </button> */}
+
+      <button style={{width: 200, height: 200, background: 'green'}} onClick={generateReportTemplateExcel}> 
+      Generate Report Template
+      </button>
     </div>
   );
 }
